898ae2fb7fe4517e535ab17b9ace9abf892368e9
june-yamamoto <attsu.gx@gmail.com>
2025-10-26 15:00:28 +0000

    ci: 修正

--- packages/frontend/.storybook/preview.tsx
+++ packages/frontend/.storybook/preview.tsx
@@ -10,7 +10,7 @@ initialize({
   serviceWorker: {
     // GitHub Pagesでサブディレクトリにデプロイされるため、
     // Service Workerのパスを環境変数から動的に設定する
-    url: `${process.env.BASE_URL || ''}mockServiceWorker.js`,
+    url: `${import.meta.env.BASE_URL}mockServiceWorker.js`,
   },
 });
 
